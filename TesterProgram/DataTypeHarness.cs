b50fc1f8bc6d51236729f783e5ce316079129a9a
Nicholas Zuniga <Nicholas.M.Zuniga@outlook.com>
2020-09-01 16:51:07 -0500

    Created Student

--- TesterProgram/DataTypeHarness.cs
+++ TesterProgram/DataTypeHarness.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassesLibrary;
 
 namespace TesterProgram
 {
-    class Program
+    class DataTypeHarness
     {
         static void Main(string[] args)
         {
+            Students s1 = new Students("Emily", "Briggs", "1001", 3.2f);
+
+            Console.WriteLine(s1);
+
         }
     }
 }
